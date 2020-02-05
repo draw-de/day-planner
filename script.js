45c693b177e4f8efe81bf4dd51b3cadeb45f235c
mdrawde <mdrawde@outlook.com>
2020-02-05 17:36:08 -0500

    fixes

--- script.js
+++ script.js
@@ -1,42 +1,44 @@
-//Shows today's date and time
-$("#date").text(moment().format("LLLL"));
+window.onload = function() {
+  //Shows today's date and time
+  $("#date").text(moment().format("LLLL"));
 
-//Making a variable to grab the time of the hour from moment.js
-var time = moment().format("H");
+  //Making a variable to grab the time of the hour from moment.js
+  var time = moment().format("H");
 
-//This is just to check the time variable
-console.log(time);
+  //This is just to check the time variable
+  console.log(time);
 
-// If statements to check if it's in that certain time block
+  // If statements to check if it's in that certain time block
 
-//You're grabbing each input then doing a callback function for element
-$("input").each(function(index, element) {
-  //Making an if statement saying if time of the hour is greater than attribute number then make it turn red
-  if (time > parseInt(hour)) {
-    $(this).css("background-color", "#f5d7d9");
-  }
-  //Similar to the first if statement, but just making it equal to the attribute number to turn it green.
-  if (time == parseInt(hour)) {
-    $(this).css("background-color", "#d0e3c8");
-  }
-  if (time < parseInt(hour)) {
-    $(this).css("background-color", "#f0dfb4");
-  }
-});
+  //You're grabbing each input then doing a callback function for element
+  $("input").each(function(index, element) {
+    //Making an if statement saying if time of the hour is greater than attribute number then make it turn red
+    var hour = $(this).attr("hour");
 
-//Save functions
-$(".save").on("click", function() {
-  $("input").each(function() {
-    var id = $(this).attr("id");
-    var value = $(this).val();
-    localStorage.setItem(id, value);
+    if (time > parseInt(hour)) {
+      $(this).css("background-color", "#f5d7d9");
+    }
+    //Similar to the first if statement, but just making it equal to the attribute number to turn it green.
+    if (time == parseInt(hour)) {
+      $(this).css("background-color", "#d0e3c8");
+    }
+    if (time < parseInt(hour)) {
+      $(this).css("background-color", "#f0dfb4");
+    }
   });
-});
 
-//Loads the saved values
-function savedInput() {
+  //Save functions
+  $(".save").on("click", function() {
+    $("input").each(function() {
+      var id = $(this).attr("id");
+      var value = $(this).val();
+      localStorage.setItem(id, value);
+    });
+  });
+
+  //Loads the saved values
   $("input").each(function() {
     var id = $(this).attr("id");
     $(this).val(localStorage.getItem(id));
   });
-}
+};
